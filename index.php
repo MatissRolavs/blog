cae67925efedf7f832b4e1e80e060a5250bd2dbd
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-02-26 09:50:36 +0200

    epic

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 
-echo "hello";
+$connection_string = "mysql:host=localhost;dbname=blog_matiss;user=root;password=;charset=utf8mb4";
+$pdo = new PDO($connection_string);
 
+//Sagatavo sql izpildei
+$query = $pdo->prepare("SELECT * FROM posts");
+//Izpildit sql
+$query->execute();
+$posts = $query->fetchAll(PDO::FETCH_ASSOC);
+
+echo "<pre>";
+var_dump($posts);
+echo "</pre>";
 ?>
