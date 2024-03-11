e1e8befbf0c901d7ac835761da6cd20b48308edd
Matiss Rolavs <matissrolavs@Matisss-MacBook-Air.local>
2024-03-11 09:46:20 +0200

    epic

--- index.php
+++ index.php
@@ -8,13 +8,13 @@ $config = require("config.php");
 $query = "SELECT * FROM posts JOIN categories ON posts.category_id = categories.id";
 $params = [];
 
-if(isset($_GET["id"])&&$_GET["id"]!=""){
-    $id = ($_GET["id"]);
-    $query .= " WHERE id=:id";
+if(isset($_GET["id"]) && $_GET["id"]!=""){
+    $id = trim($_GET["id"]);
+    $query .= " WHERE posts.id=:id";
     $params = [":id" => $id];
 }
 elseif(isset($_GET["category"])&&$_GET["category"]!=""){
-    $category = ($_GET["category"]);
+    $category = trim($_GET["category"]);
     $query .= " WHERE name=:category";
     $params = [":category" => $category];
 }
@@ -24,12 +24,12 @@ $db = new Db($config);
 $posts = $db->execute($query,$params)->fetchAll();
 
 echo "<form>";
-echo "<input name='id'/>";
+echo "<input name='id' value='" . ($id ?? '') ." '/>";
 echo "<button>Submit ID</button>";
 echo "</form>";
 
 echo "<form>";
-echo "<input name='category'/>";
+echo "<input name='category' value='" . ($category ?? '') ." '/>";
 echo "<button>Submit Category</button>";
 echo "</form>";
 
